fa22c0c0cf0cdfacde2fee461981114648197c35
Aserturik <alex.hernandez27@uptc.edu.co>
2025-09-14 15:56:04 -0500

    interfaces

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,25 +1,24 @@
-const ironman = {
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+
+  address: Adress;
+}
+
+interface Adress {
+  postalCode: string;
+  city: string;
+}
+
+const ironman: Person = {
   firstName: "Tony",
   lastName: "Stark",
-  age: 34,
+  age: 45,
   address: {
-    postalCode: 222,
+    postalCode: "ABC123",
     city: "New York",
   },
 };
 
-// esto copia la referencia a primer nivel (solo los atributos de primer nivel por que si cambio spiderman.address.postalCode = 333, para ironman también cambia)
-// const spiderman = { ...ironman };
-
-// esto si hace un clon completo del objeto y sus objetos hijos
-// DeepClone
-const spiderman = structuredClone(ironman);
-
-spiderman.firstName = "Peter";
-spiderman.lastName = "Parker";
-spiderman.age = 22;
-
-spiderman.address.city = "Philadelphia";
-
 console.log(ironman);
-console.log(spiderman);
